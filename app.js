8a1edc2aeb696cc976451a871c77ce3df88d570a
Gustavo Jordao <gustavo.jordao021@gmail.com>
2020-03-28 16:24:23 -0400

    Setup CORS, loggedIn check

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const express      = require('express');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const cors         = require('cors');
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -20,7 +21,12 @@ app.use(cookieParser());
 
 require('./configs/db.config');
 require('./configs/session.config')(app);
-require('./configs/passport/passport.config.js')(app);  
+require('./configs/passport/passport.config.js')(app);
+
+app.use(cors({
+    origin: 'localhost:3000',
+    credentials: true
+}))
 
 // Route setup
 app.use('/', require('./routes/index'));
