4264cdaacf4e33585ae5362095c5d0911574c40a
Gustavo Jordao <gustavo.jordao021@gmail.com>
2020-04-18 13:41:41 -0400

    Work on toggling goal form off, updating user

--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const passport = require('passport');
-const User = require('../models/User.model');
+const mongoose = require("mongoose");
+const passport = require("passport");
+const User = require("../models/User.model");
 
-const bcryptjs = require('bcryptjs');
+const bcryptjs = require("bcryptjs");
 const saltRounds = 10;
 
-const routeGuard = require('../configs/route-guard.config');
+const routeGuard = require("../configs/route-guard.config");
 
-router.post('/signup', (req, res, next) => {
+router.post("/signup", (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (!username || !email || !password) {
     res.status(401).json({
-      errorMessage: 'All fields are mandatory. Please provide your username, email and password.'
+      errorMessage:
+        "All fields are mandatory. Please provide your username, email and password.",
     });
     return;
   }
@@ -23,40 +24,44 @@ router.post('/signup', (req, res, next) => {
   if (!regex.test(password)) {
     res.json({
       errorMessage:
-        'Password needs to have at least 8 characters, and must contain at least one number, one lowercase and one uppercase letter.'
+        "Password needs to have at least 8 characters, and must contain at least one number, one lowercase and one uppercase letter.",
     });
     return;
   }
 
   bcryptjs
     .genSalt(saltRounds)
-    .then(salt => bcryptjs.hash(password, salt))
-    .then(hashedPassword => {
+    .then((salt) => bcryptjs.hash(password, salt))
+    .then((hashedPassword) => {
       return User.create({
         username,
         email,
-        passwordHash: hashedPassword
+        passwordHash: hashedPassword,
       })
-        .then(user => {
-          req.login(user, err => {
-            if (err) return res.json({ errorMessage: 'Something went wrong with login!' });
+        .then((user) => {
+          req.login(user, (err) => {
+            if (err)
+              return res.json({
+                errorMessage: "Something went wrong with login!",
+              });
             user.passwordHash = undefined;
-            res.json({ successMessage: 'Login successful!', user });
+            res.json({ successMessage: "Login successful!", user });
           });
         })
-        .catch(err => {
+        .catch((err) => {
           if (err instanceof mongoose.Error.ValidationError) {
             res.json({ errorMessage: "test error" });
           } else if (err.code === 11000) {
             res.json({
-              errorMessage: 'Username and email need to be unique. Either username or email is already used.'
+              errorMessage:
+                "Username and email need to be unique. Either username or email is already used.",
             });
           } else {
             next(err);
           }
         });
     })
-    .catch(err => next(err));
+    .catch((err) => next(err));
 });
 
 router.post("/login", (req, res, next) => {
@@ -65,34 +70,44 @@ router.post("/login", (req, res, next) => {
       res
         .status(200)
         .json({ errorMessage: "Something went wrong with database query" });
-    }
-    else if (!user) {
-      res.status(200).json({errorMessage: failureDetails.message});
-    }
-    else {
-      req.login(user, err => {
+    } else if (!user) {
+      res.status(200).json({ errorMessage: failureDetails.message });
+    } else {
+      req.login(user, (err) => {
         if (err) {
-          return res
-            .json({ errorMessage: "Something went wrong with login!" })}
+          return res.json({ errorMessage: "Something went wrong with login!" });
+        }
         user.passwordHash = undefined;
-        res.status(200).json({successMessage: "Login Successful!", user});
+        res.status(200).json({ successMessage: "Login Successful!", user });
       });
     }
   })(req, res, next);
 });
 
-router.post('/logout', (req, res, next) => {
+router.post("/logout", (req, res, next) => {
   req.logout();
-  res.status(200).json({ message: 'Logout successful!' });
+  res.status(200).json({ message: "Logout successful!" });
 });
 
-router.get('/isLoggedIn', (req, res) => {
+// router.get("/isLoggedIn", (req, res) => {
+//   if (req.user) {
+//     req.user.passwordHash = undefined;
+//     res.json({ user: req.user });
+//     return;
+//   }
+//   res.json({ errorMessage: "Unauthorized access!" });
+
+router.get("/isLoggedIn", (req, res) => {
   if (req.user) {
-    req.user.passwordHash = undefined;
-    res.json({ user: req.user });
-    return;
+    User.findById(req.user._id)
+      .then((userFound) => {
+        req.user.passwordHash = undefined;
+        console.log(userFound);
+        res.json({ user: userFound });
+        return;
+      })
+      .catch((err) => res.json({ errorMessage: "Unauthorized access!" }));
   }
-  res.json({ errorMessage: 'Unauthorized access!' });
 });
 
 module.exports = router;
