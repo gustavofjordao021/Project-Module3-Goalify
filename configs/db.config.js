2b119b82008def596559e2739e222323361a3585
Gustavo Jordao <gustavo.jordao021@gmail.com>
2020-05-06 19:26:42 -0400

    initial to commit heroku

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,11 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose
-  .connect('mongodb://localhost/project-module3-goalify', {
+  .connect(process.env.MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
+    useFindAndModify: false,
   })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
+  });
