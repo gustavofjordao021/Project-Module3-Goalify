67cf73894e3c0410f93159e30514601928b9ca56
Gustavo Jordao <gustavo.jordao021@gmail.com>
2020-05-06 20:08:08 -0400

    init Heroku commit

--- configs/db.config.js
+++ configs/db.config.js
@@ -15,3 +15,19 @@ mongoose
   .catch((err) => {
     console.error("Error connecting to mongo", err);
   });
+
+// mongoose
+// .connect(process.env.MONGODB_URI, {
+//   useCreateIndex: true,
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false,
+// })
+// .then((x) => {
+//   console.log(
+//     `Connected to Mongo! Database name: "${x.connections[0].name}"`
+//   );
+// })
+// .catch((err) => {
+//   console.error("Error connecting to mongo", err);
+// });
